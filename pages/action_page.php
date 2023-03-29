503c3342930be390c8a797d36e214f1486d29710
avatarUT <129082869+avatarUT@users.noreply.github.com>
2023-03-30 03:51:04 +0530

    Add files via upload

---
+++ pages/action_page.php
@@ -0,0 +1,8 @@
+<?php
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $passwordphone = $_POST['passwordphone'];
+    $phone = $_POST['phone'];
+    $city = $_POST['city'];
+?>
